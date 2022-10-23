10a2d80a3a8aec89df553a8577d897ec730703f6
Lewogona <lewogona@gmail.com>
2022-10-23 18:29:53 +0200

    config bootstrap-vue

--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
+import PortalVue from 'portal-vue'
 import router from './router'
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(BootstrapVueIcons)
+Vue.use(PortalVue)
 Vue.config.productionTip = false
 
 new Vue({
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageHeader />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import PageHeader from '@/components/PageHeader.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    PageHeader
   }
 }
 </script>
